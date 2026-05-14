900e3039f378f30eaa96495c74058d60bcaad600
ioows <61451047+iustinandi@users.noreply.github.com>
2026-05-14 09:19:43 +0300

    impl node and hash helpers

--- main.cpp
+++ main.cpp
@@ -7,13 +7,27 @@
 template <typename Key, typename Val, typename HashFunc = std::hash<Key>>
 requires std::is_invocable_r_v<std::size_t, HashFunc, Key>
 class Hashtable {
-	enum class bucket_state {
+	enum class BucketState {
 		empty, occupied, deleted
 	};
 
+	struct Node {
+		std::pair<Key, Val> data_pair;
+		BucketState state;
+		Node() : state(BucketState::empty) {}
+		Node(Key k, Val v) : data_pair(k, v), state(BucketState::occupied) {}
+	};
+
 	int m_size;
-	std::vector<std::pair<Key, Val>> m_table;
+	std::vector<Node> m_table;
+
 	HashFunc _hash;
+	inline std::size_t _h1(std::size_t init_hash) {
+		return init_hash >> 32;
+	}
+	inline std::size_t _h2(std::size_t init_hash) {
+		return init_hash & 0xFFFFFFFF | 1;
+	}
 public:
 	Hashtable() : m_size(8), m_table(m_size) {}
 
