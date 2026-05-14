41da09230c8c3d095f83687aa5d26e25ae1ac561
ioows <61451047+iustinandi@users.noreply.github.com>
2026-05-14 11:12:51 +0300

    impl rehashing + checks

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ class Hashtable {
 	inline std::size_t _h2(std::size_t init_hash) {
 		return init_hash & 0xFFFFFFFF | 1;
 	}
-	
+
 	int _get_index(Key k) {
 		std::size_t hash = _hash(k);
 		std::size_t h1 = _h1(hash);
@@ -41,9 +41,31 @@ class Hashtable {
 			index = (index + h2) % m_size;
 		return index;
 	}
+
+	void _rehash() {
+		std::vector<Node> old_table = std::move(m_table);
+
+		m_size <<= 1; // TODO: check if is this even an improvement over *= 2 or if it's just larp
+		m_table = std::vector<Node>(m_size);
+		m_count_occupied = m_count_deleted = 0;
+
+		for (const Node& node : old_table) {
+			if (node.state == BucketState::occupied) {
+				insert(node.data_pair.first, node.data_pair.second);
+			}
+		}
+	}
+
+	void _check() {
+		if (static_cast<double>(m_count_occupied) / m_size > 0.7 // factorul de incarcare
+			|| static_cast<double>(m_count_deleted) / m_size > 0.1) // % contaminare
+			_rehash();
+	}
+
 public:
 	Hashtable() : m_size(8), m_table(m_size) {}
 	~Hashtable() {}
+
 	void insert(Key k, Val v) { // TODO: rehash
 		int index = _get_index(k);
 		Node& node = m_table[index];
@@ -55,7 +77,10 @@ public:
 
 		node.state = BucketState::occupied;
 		m_count_occupied++;
+
+		_check();
 	}
+
 	std::pair<Key, Val>* find(Key k) { // TODO: replace with iterator
 		int index = _get_index(k);
 		Node& node = m_table[index];
@@ -65,6 +90,7 @@ public:
 		else
 			return nullptr;
 	}
+
 	void remove(Key k) {
 		int index = _get_index(k);
 		Node& node = m_table[index];
@@ -74,6 +100,8 @@ public:
 			
 			m_count_occupied--;
 			m_count_deleted++;
+
+			_check();
 		}
 	}
 
